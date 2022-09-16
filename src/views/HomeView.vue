091739cb57a24c0d05670dcf81965dc7fa75aabd
james-rutter-corbett <james.rutter-corbett@edenpetfoods.com>
2022-09-16 02:00:49 +0100

    Added new project and changed fa icons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -29,35 +29,32 @@
 
             <div class="row">
               <div class="col-sm-12 col-md-12 col-lg-5 text-order">
-                <a class="primary-link" href="https://crimson-coffee.jrc-web.co.uk" target="_blank">Crimson Coffee</a>
+                <a class="primary-link" href="/api/" target="_blank">API</a>
                 <div class="description-box p-4 mt-4">
-                  <span class="text-white">This is a concept webpage for a fictional coffee company.</span>
+                  <span class="text-white">A dynamic API made from scratch using php.</span>
                 </div>
 
                 <div class="mt-3 mb-2 language-attributes-left">
-                  <a href="#!" title="HTML5"><i class="fab fa-html5 fa-5x project-attribute-icons"></i></a>
-                  <a href="#!" title="CSS3"><i class="fab fa-css3-alt fa-5x project-attribute-icons"></i></a>
                   <a href="#!" title="jQuery" class="jQuery">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="5em" height="4.5em" viewBox="-0.213 -0.514 285.74387 283.47476">
-                      <path fill="#1b75bb" d="M275 210c-42.2 46.2-120 51.2-181.5 9.3C30 176 7 98.3 38.8 41.6c-6.7 6-12.7 13-18 20.7C-20.2 122.8 1 209 68 255c67.3 46 155.3 34 196.6-26.4 4-6 7.5-12.2 10.4-18.6zm10.5-76.7l-1.8 2.7c-27.4 37.3-86.3 42.6-133.5 11.2C102 115 84.2 56.7 110.7 17c2-3 4.3-6 6.7-8.6l6-6-6.6 6c-5 5-10 10.8-14 17-33 49.4-19.5 116 29.8 148.8 49.3 32.8 115 18.8 147.8-30.5 0 0 3-5 5-10.4zm-100.8-32.6c27.8 19 64 15.6 84.7-6.6 2-2 4-4 5.7-7 3-4 5-8 7-12-.4 1.5-1.3 3-2.2 4-3 4.8-7 8.7-11.7 11.8-19 12.7-48 11.5-72-4.8-26.6-18.3-38-49-28.4-72.6 1-3 2.6-5.5 4.3-8 1.2-2 2.7-4 4.3-5.4-4.2 3.2-8 7.3-11 12l-.5.5c-19 28.5-9.7 68 20.5 88.5z" class="jquery__wave"></path>
+                    <svg class="php__svg" xmlns="http://www.w3.org/2000/svg" width="115px" viewBox="0 0 640 512">
+                      <path d="M320 104.5c171.4 0 303.2 72.2 303.2 151.5S491.3 407.5 320 407.5c-171.4 0-303.2-72.2-303.2-151.5S148.7 104.5 320 104.5m0-16.8C143.3 87.7 0 163 0 256s143.3 168.3 320 168.3S640 349 640 256 496.7 87.7 320 87.7zM218.2 242.5c-7.9 40.5-35.8 36.3-70.1 36.3l13.7-70.6c38 0 63.8-4.1 56.4 34.3zM97.4 350.3h36.7l8.7-44.8c41.1 0 66.6 3 90.2-19.1 26.1-24 32.9-66.7 14.3-88.1-9.7-11.2-25.3-16.7-46.5-16.7h-70.7L97.4 350.3zm185.7-213.6h36.5l-8.7 44.8c31.5 0 60.7-2.3 74.8 10.7 14.8 13.6 7.7 31-8.3 113.1h-37c15.4-79.4 18.3-86 12.7-92-5.4-5.8-17.7-4.6-47.4-4.6l-18.8 96.6h-36.5l32.7-168.6zM505 242.5c-8 41.1-36.7 36.3-70.1 36.3l13.7-70.6c38.2 0 63.8-4.1 56.4 34.3zM384.2 350.3H421l8.7-44.8c43.2 0 67.1 2.5 90.2-19.1 26.1-24 32.9-66.7 14.3-88.1-9.7-11.2-25.3-16.7-46.5-16.7H417l-32.8 168.7z"/>
                     </svg>
                   </a>
                 </div>
 
                 <div class="px-3 github-links">
-                  <a class="navbar-brand p-1 mx-1" href="https://crimson-coffee.jrc-web.co.uk" target="_blank">
-                    <i class="fab fa-github"></i>
-                  </a>
-                  <a class="navbar-brand p-1 mx-1" href="https://github.com/Verhiron/Crimson-Coffee-Webpage" target="_blank">
-                    <i class="fa fa-code"></i>
-                  </a>
+<!--                  <a class="navbar-brand p-1 mx-1" href="https://matterhorn.jrc-web.co.uk" target="_blank">-->
+<!--                    <i class="fab fa-github"></i>-->
+<!--                  </a>-->
+                <a class="navbar-brand p-1 mx-1" href="/api/" target="_blank">
+                  <i class="fas fa-desktop"></i>
+                </a>
                 </div>
 
               </div>
               <div class="col-sm-12 col-md-12 col-lg-7 image-order" style="text-align: right;">
-
-                <a href="https://crimson-coffee.jrc-web.co.uk" target="_blank">
-                  <img width="100%" class="img-border" src="/src/assets/images/Crimson-Coffee.png">
+                <a href="/api/" target="_blank">
+                  <img class="img-border" width="100%" src="/src/assets/images/api-php.png">
                 </a>
               </div>
             </div>
@@ -71,22 +68,60 @@
 
             <div class="row">
               <div class="col-sm-12 col-md-12 col-lg-7 image-order" style="text-align: right;">
-                <a href="https://matterhorn.jrc-web.co.uk" target="_blank">
-                  <img class="img-border" width="100%" src="https://user-images.githubusercontent.com/70725429/130340200-e2a60737-a3e2-4a11-b182-c371debc9896.gif">
+                <a href="https://crimson-coffee.jrc-web.co.uk" target="_blank">
+                  <img width="100%" class="img-border" src="/src/assets/images/Crimson-Coffee.png">
                 </a>
               </div>
 
               <div class="col-sm-12 col-md-12 col-lg-5 text-order">
 
                 <div class="links-right">
-                  <a class="primary-link" href="https://matterhorn.jrc-web.co.uk" target="_blank">The Matterhorn</a>
+                  <a class="primary-link" href="https://crimson-coffee.jrc-web.co.uk" target="_blank">Crimson Coffee</a>
                 </div>
 
                 <div class="description-box-reverse p-4 mt-4">
-                  <span class="text-white">A fully responsive webpage for the Matterhorn Mountain.</span>
+                  <span class="text-white">This is a concept webpage for a fictional coffee company.</span>
                 </div>
 
                 <div class="mt-3 mb-2 language-attributes-right">
+                  <a href="#!" title="HTML5"><i class="fab fa-html5 fa-5x project-attribute-icons"></i></a>
+                  <a href="#!" title="CSS3"><i class="fab fa-css3-alt fa-5x project-attribute-icons"></i></a>
+                  <a href="#!" title="jQuery" class="jQuery">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="5em" height="4.5em" viewBox="-0.213 -0.514 285.74387 283.47476">
+                      <path fill="#1b75bb" d="M275 210c-42.2 46.2-120 51.2-181.5 9.3C30 176 7 98.3 38.8 41.6c-6.7 6-12.7 13-18 20.7C-20.2 122.8 1 209 68 255c67.3 46 155.3 34 196.6-26.4 4-6 7.5-12.2 10.4-18.6zm10.5-76.7l-1.8 2.7c-27.4 37.3-86.3 42.6-133.5 11.2C102 115 84.2 56.7 110.7 17c2-3 4.3-6 6.7-8.6l6-6-6.6 6c-5 5-10 10.8-14 17-33 49.4-19.5 116 29.8 148.8 49.3 32.8 115 18.8 147.8-30.5 0 0 3-5 5-10.4zm-100.8-32.6c27.8 19 64 15.6 84.7-6.6 2-2 4-4 5.7-7 3-4 5-8 7-12-.4 1.5-1.3 3-2.2 4-3 4.8-7 8.7-11.7 11.8-19 12.7-48 11.5-72-4.8-26.6-18.3-38-49-28.4-72.6 1-3 2.6-5.5 4.3-8 1.2-2 2.7-4 4.3-5.4-4.2 3.2-8 7.3-11 12l-.5.5c-19 28.5-9.7 68 20.5 88.5z" class="jquery__wave"></path>
+                    </svg>
+                  </a>
+                </div>
+
+                <div class="links-right">
+                  <a class="navbar-brand p-1 mx-1" href="https://github.com/Verhiron/Crimson-Coffee-Webpage" target="_blank">
+                    <i class="fab fa-github"></i>
+                  </a>
+                  <a class="navbar-brand p-1 mx-1" href="https://crimson-coffee.jrc-web.co.uk" target="_blank">
+                    <i class="fas fa-desktop"></i>
+                  </a>
+                </div>
+
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row mt-xlg gx-5">
+        <div class="col">
+          <div class="col p-2">
+
+            <div class="row">
+              <div class="col-sm-12 col-md-12 col-lg-5 text-order">
+                <a class="primary-link" href="https://matterhorn.jrc-web.co.uk" target="_blank">The Matterhorn</a>
+                <div class="description-box p-4 mt-4">
+                  <span class="text-white">A fully responsive webpage for the Matterhorn Mountain.</span>
+                </div>
+
+                <div class="mt-3 mb-2 language-attributes-left">
                   <a href="#!" title="HTML5"><i class="fab fa-html5 fa-5x project-attribute-icons"></i></a>
                   <a href="#!" title="Sass"><i class="fab fa-sass fa-5x project-attribute-icons"></i></a>
                   <a href="#!" title="jQuery" class="jQuery">
@@ -96,18 +131,21 @@
                   </a>
                 </div>
 
-                <div class="links-right">
-                  <a class="navbar-brand p-1 mx-1" href="https://matterhorn.jrc-web.co.uk" target="_blank">
+                <div class="px-3 github-links">
+                  <a class="navbar-brand p-1 mx-1" href="https://github.com/Verhiron/The-Matterhorn-Webpage" target="_blank">
                     <i class="fab fa-github"></i>
                   </a>
-                  <a class="navbar-brand p-1 mx-1" href="https://github.com/Verhiron/The-Matterhorn-Webpage" target="_blank">
-                    <i class="fa fa-code"></i>
+                  <a class="navbar-brand p-1 mx-1" href="https://matterhorn.jrc-web.co.uk" target="_blank">
+                    <i class="fas fa-desktop"></i>
                   </a>
                 </div>
 
-
               </div>
-
+              <div class="col-sm-12 col-md-12 col-lg-7 image-order" style="text-align: right;">
+                <a href="https://matterhorn.jrc-web.co.uk" target="_blank">
+                  <img class="img-border" width="100%" src="https://user-images.githubusercontent.com/70725429/130340200-e2a60737-a3e2-4a11-b182-c371debc9896.gif">
+                </a>
+              </div>
             </div>
           </div>
         </div>
@@ -145,7 +183,7 @@
     margin: 12px;
   }
 
-  .fa-github, .fa-code{
+  .fa-github, .fa-code, .fa-desktop{
     color: #682860;
   }
 
@@ -153,13 +191,25 @@
     font-size: 1.5em;
   }
 
-  .fa-github:hover, .fa-code:hover{
+  .fa-desktop{
+    font-size: 1.3em;
+  }
+
+  .fa-github:hover, .fa-code:hover, .fa-desktop:hover{
     color: #fff;
     /*682860*/
   }
 
   .jquery__wave {
     fill: #e9e9e9;
+  }
+
+  .php__svg{
+    fill: #fff;
+  }
+
+  .php__svg:hover{
+    fill: #8993be;
   }
 
   .fa-html5:hover{
